59fde54a1bea2d86b493186da4788ac860d8c3cc
Paul Menhart <paul@menhart.net>
2021-12-08 18:20:09 +0100

    add some tutorial stuff..

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,45 @@
 <script lang="ts">
+	import Nested from "./Nested.svelte";
 	export let name: string;
+	let countA = 1;
+	let countB = 1;
+	let numbers = [];
+	function incrementCountA() {
+		countA += 1;
+	}
+	function incrementCountB() {
+		countB += 1;
+	}
+	function addNumbers() {
+		numbers[numbers.length] = " " + (numbers.length + 1);
+		numbers[numbers.length] = " " + (numbers.length + 1);
+		numbers = numbers.slice(1)
+	}
+	$: {
+		console.log(`countA is ${countA}\n countB is ${countB}`);
+	}
 </script>
 
 <main>
+	<button on:click={incrementCountA}> Add to A </button>
+	{#if countA > numbers.length}
+		<button on:click={incrementCountB}> Add to B </button>
+	{/if}
+	<button on:click={addNumbers}> Push to list </button>
+	<Nested prop={1234} />
+	<p>list: {numbers}</p>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		A is {countA}
+	</p>
+	<p>
+		B is {countB}
+	</p>
+	<Nested prop="new text" />
 </main>
 
 <style>
@@ -20,6 +55,12 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	p {
+		color: purple;
+		font-family: "Comic Sans MS", cursive;
+		font-size: 2em;
 	}
 
 	@media (min-width: 640px) {
---
+++ src/Nested.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let prop;
+</script>
+
+<p>Hello from nested.svelte</p>
+<p>This has a {prop} property</p>
