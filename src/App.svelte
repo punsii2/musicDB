03ddfa04050873970d54271ee8914c17da13e61f
Paul Menhart <paul@menhart.net>
2021-12-12 13:10:01 +0100

    add svelte-routing. Does not work with sveltestrap navlinks yet.

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,50 @@
 <script lang="ts">
+    import { Router, Link, Route } from "svelte-routing";
     import { Styles } from "sveltestrap";
-    import { TabContent, TabPane } from "sveltestrap";
-    export let name: string;
+    import { Nav, NavItem, NavLink } from "sveltestrap";
 
     import PersonSearch from "./PersonSearch.svelte";
     import persons from "./testdata/persons.json";
     import WorkSearch from "./WorkSearch.svelte";
     import works from "./testdata/works.json";
+
+    // needed for routing
+    //export let to = "";
+    //function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
+    //    const isActive =
+    //        href === "/" ? isCurrent : isPartiallyCurrent || isCurrent;
+
+    //    // The object returned here is spread on the anchor element's attributes
+    //    if (isActive) {
+    //        return { class: "active" };
+    //    }
+    //    return {};
+    //}
+    export let url = "";
+    export let name: string;
 </script>
 
-<main>
+<Router {url}>
     <Styles />
+    <Nav tabs>
+        <NavItem>
+            <Link to="/"> {name} </Link>
+        </NavItem>
+        <NavItem>
+            <Link to="Person"> Person </Link>
+        </NavItem>
+        <NavItem>
+            <Link to="Work">Work</Link>
+        </NavItem>
+    </Nav>
 
-    <TabContent>
-        <TabPane tabId="music" tab={name}>Hello {name}</TabPane>
-        <TabPane tabId="persons" tab="Persons" active>
-            <PersonSearch {persons} />
-        </TabPane>
-        <TabPane tabId="works" tab="Works">
-            <WorkSearch {works} />
-        </TabPane>
-    </TabContent>
-</main>
+    <main>
+        <Route path="Person" component={PersonSearch} {persons} />
+        <!--<Route path="Person/:id" component={Person} />-->
+        <Route path="Work" component={WorkSearch} />
+        <!--<Route path="Work/:id" component={WorkSearch} />-->
+    </main>
+</Router>
 
 <style>
     :global(body) {
